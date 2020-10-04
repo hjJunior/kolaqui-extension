12c57a2804b8c2b335ea541a5465fc86c760e7bf
Helio S. Junior <hj_junior@hotmail.com>
2020-10-04 17:23:54 -0300

    Adds uploadfile feature

--- background.js
+++ background.js
@@ -4,26 +4,6 @@
 
 'use strict';
 
-// const baseUrl = 'https://kolaqui.herokuapp.com/api';
-const baseUrl = 'https://kolaqui.herokuapp.com/api';
-
-const sendToServer = ({path, method, body}, callback) => {
-  return fetch(
-    `${baseUrl}/${path}`,
-    {
-      method,
-      body,
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    }
-  ).then((result) => {
-    debugger;
-    callback(result.body);
-  });
-}
-
-
 chrome.runtime.onInstalled.addListener(function() {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
@@ -32,17 +12,5 @@ chrome.runtime.onInstalled.addListener(function() {
       })],
       actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
-  });
-});
-
-chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
-  console.log('Pushing to server: ', request);
-  debugger;
-
-  sendToServer({
-    ...request
-  }, (payload) => {
-    debugger;
-    chrome.runtime.sendMessage(sender.tab.id, payload);
   });
 });
--- spyReview.js
+++ spyReview.js
@@ -1,60 +1,22 @@
 (function () {
+  const baseUrl = 'https://kolaqui.herokuapp.com/api';
+  // const baseUrl = 'http://127.0.0.1:8000/api';
+
   console.log('Capturing from exame result page!');
 
-  const questionSectionSelector = '.que';
-  const questionTextSelector = '.qtext';
-  const gradeSelector = '.grade';
-  const checkboxAnswerSelectedSelector = '.answer input:radio:checked'
-  const exameResult = '#region-main > div:nth-child(2) > table > tbody > tr:nth-child(5) > td';
-  const courseSlug = '#page-navbar > nav > ol > li:nth-child(3)';
-  
-  const getReplyForMultichoiceQuestion = (questionElement) => {
-    const answer = $(questionElement).find(checkboxAnswerSelectedSelector);
-    const answerId = $(answer).attr('name');
-    const isCorrect = isQuestionCorrect(questionElement);
-  
-    return { answerId, isCorrect };
+  const submitPageHtml = ()  => {
+    const formData = new FormData();
+    const blob = new Blob([
+      `<html>${$("html").html()}</html>`
+    ], { type: 'text/html' });
+
+    formData.append('file', blob, 'exame-review.html');
+
+    var request = new XMLHttpRequest();
+    request.open('POST', `${baseUrl}/upload`);
+    request.send(formData);
   }
 
-  const isQuestionCorrect = (questionElement) => {
-    const regex = /Atingiu (\d+\,\d+) de (\d+\,\d+)/;
-    const gradeText = $(questionElement).find(gradeSelector).text();
-    const matchResult = gradeText.match(regex);
-
-    return matchResult ? matchResult[1] === matchResult[2] : false;
-  }
-  
-  const getRepliesForQuestion = (questionElement) => {
-    return getReplyForMultichoiceQuestion(questionElement);
-  }
-
-  const getAttemptId = () => new URLSearchParams(window.location.search).get('attempt');
-  
-  const getReplies = () => {
-    const exameQuestions = [];
-  
-    $(questionSectionSelector).each((index, questionElement) => {
-      const questionId = questionElement.id;
-    
-      exameQuestions.push({
-        questionId,
-        ...getRepliesForQuestion(questionElement),
-      });
-    });
-  
-    return exameQuestions;
-  }
-  
-  const getExame = () => {
-    return {
-      exameResult: $(exameResult).text(),
-      courseSlug: $(courseSlug).text(),
-      replies: getReplies(),
-      attemptId: getAttemptId(),
-    }
-  }
-
-  const body = JSON.stringify(getExame());
-  chrome.runtime.sendMessage({ path: 'exame-review', method: 'post', body });
+  submitPageHtml();
 })();
 
